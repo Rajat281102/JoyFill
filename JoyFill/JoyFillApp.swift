ba57ff909be4377ace021a36adfd7f7e09b67764
Vivek <vivekjyani11@gmail.com>
2024-02-10 13:19:03 +0530

    Initial Commit

---
+++ JoyFill/JoyFillApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyFillApp.swift
+//  JoyFill
+//
+//  Created by Vikash on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JoyFillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
